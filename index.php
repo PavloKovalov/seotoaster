c4e5218baabb1ef4794402ee94e159beff8e7498
Pavel Kovalyov <sxe.funky@gmail.com>
2014-05-07 18:53:10 +0300

    Move installer into core :smirk:

--- index.php
+++ index.php
@@ -1,53 +1,52 @@
 <?php
-/*************************************************************************
- * Necessary alter for pcre.backtrack_limit
- *
- ************************************************************************/
+// Necessary alter for pcre.backtrack_limit
 ini_set('pcre.backtrack_limit', '10000000000000');
 
 
-/*************************************************************************
- * Installation check
- *
- ************************************************************************/
+// Installation check
+//if (is_dir(realpath(dirname(__FILE__) . '/install/'))) {
+//    header('Location: install/');
+//    exit();
+//}
+// End installation check
 
-if(is_dir(realpath(dirname(__FILE__).'/install/'))){
-	header('Location: install/');
-	exit();
+
+// Reading current core config
+$coreConfigPath = realpath(__DIR__ . '/system/coreinfo.php');
+
+if (file_exists($coreConfigPath)) {
+    require_once realpath($coreConfigPath);
 }
 
-/* End installation check */
-
-
-/*************************************************************************
- * Reading current core config
- *
- *************************************************************************/
-$coreConfigPath = realpath(dirname(__FILE__).'/system/coreinfo.php');
-
-if(file_exists($coreConfigPath)) {
-	require_once realpath($coreConfigPath);
-}
-
-defined('CORE')      || define('CORE', realpath(dirname(__FILE__) . '/seotoaster_core/'));
+defined('CORE') || define('CORE', __DIR__ . '/seotoaster_core/');
 defined('SITE_NAME') || define('SITE_NAME', '');
 
-/* End reading current core config */
+// End reading current core config
 
 
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', CORE . 'application');
+|| define('APPLICATION_PATH', CORE . 'application/');
 
 // Define application environment
 defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+|| define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+$libraryPath = realpath(CORE . 'library/');
+if (!$libraryPath) {
+    exit('Can not find library directory');
+}
 
 // Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
-    get_include_path(),
-)));
+set_include_path(
+    implode(
+        PATH_SEPARATOR,
+        array(
+            $libraryPath,
+            get_include_path(),
+        )
+    )
+);
 
 // header to prevent security issues in iframes (to avoid clickjacking attacks)
 header('X-Frame-Options: SAMEORIGIN');
@@ -56,9 +55,17 @@ header('X-Frame-Options: SAMEORIGIN');
 require_once 'Zend/Application.php';
 
 // Create application, bootstrap, and run
+if (SITE_NAME) {
+    $configIni = APPLICATION_PATH . 'configs' . DIRECTORY_SEPARATOR . SITE_NAME . '.ini';
+} else {
+    $configIni = APPLICATION_PATH . '/installer/installer.ini';
+    define('INSTALL_PATH', __DIR__.'/');
+}
+
 $application = new Zend_Application(
     APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/' . SITE_NAME . '.ini'
+    $configIni
 );
+
 $application->bootstrap()
-            ->run();
+    ->run();
